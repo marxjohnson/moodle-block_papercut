66a85ff7fd22e39828b2087c5e4e749ed5ad4528
Mark Johnson <mark.johnson@tauntons.ac.uk>
2011-12-21 09:31:16 +0000

    Added extra Metadata

--- version.php
+++ version.php
@@ -1,3 +1,7 @@
 <?php
 $plugin->version = 2011071401;
 ?>
+$plugin->requires = 2010112400;
+$plugin->component = 'block_papercut';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '2.0 (Build: 2011071401)';
